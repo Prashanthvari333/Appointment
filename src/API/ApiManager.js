10496b06739e2bb2f093b40a7c26874fa5779d71
Prashanthvari333 <prashanthvarikuppala333@gmail.com>
2023-11-07 14:07:29 +0530

    Api manager files is updated

---
+++ src/API/ApiManager.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ApiManager = () => {
+  return (
+    <View>
+      <Text>ApiManager</Text>
+    </View>
+  )
+}
+
+export default ApiManager
+
+const styles = StyleSheet.create({})
